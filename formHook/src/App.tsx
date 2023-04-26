9d609326c0e4f1261aa5411313b82ca4efbcd39d
everton leonel <everton_leonel@hotmail.com>
2023-04-25 23:52:36 -0400

    Test 1

---
+++ formHook/src/App.tsx
@@ -0,0 +1,66 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  nome: string;
+  senha: string;
+  confirmarSenha: string;
+  email: string;
+  confirmarEmail: string;
+};
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: {} as FormValues,
+  });
+
+  const handleSubmitData: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+  };
+
+  console.log(errors, "sou erros fo dorm");
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit(handleSubmitData)}
+        style={{ display: "flex", flexDirection: "column", gap: 20 }}
+      >
+        <input
+          {...register("nome", {
+            required: "Campo obrigatório",
+            minLength: { value: 4, message: "No minimo 4 tio" },
+          })}
+          name="nome"
+          placeholder="nome"
+        />
+
+        <input
+          {...(register("senha"), { required: "Este campo é obrigatoio" })}
+          name="senha"
+          type="password"
+          placeholder="senha"
+        />
+        <input
+          {...register("confirmarSenha")}
+          name="confirmarSenha"
+          type="password"
+          placeholder="confirmar senha"
+        />
+        <input {...register("email")} name="email" placeholder="email" />
+        <input
+          {...register("confirmarEmail")}
+          name="confirmarEmail"
+          placeholder="confirmar email"
+        />
+
+        <p>{errors && errors.confirmarSenha?.message}</p>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
